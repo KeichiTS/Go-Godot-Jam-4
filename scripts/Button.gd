c1f19a560369b9e4cc4b2b5ed27c83f333fa150d
KeichiTS <kkeichi@gmail.com>
2023-05-09 00:47:54 -0300

    door_and_button_(working)

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -1,10 +1,17 @@
+#Code by: KeichiTS - 2023
+
 extends Area2D
 
+var body_counts = 0
 
 func _on_body_entered(body):
-	if get_parent().is_in_group('door'):
-		get_parent()._open_the_door()
+	if body_counts == 0:
+		if get_parent().is_in_group('door'):
+			get_parent()._open_the_door()
+	body_counts += 1
 
 func _on_body_exited(body):
-	if get_parent().is_in_group('door'):
-		get_parent()._close_the_door()
+	body_counts -= 1
+	if body_counts == 0:
+		if get_parent().is_in_group('door'):
+			get_parent()._close_the_door()
--- scripts/door.gd
+++ scripts/door.gd
@@ -1,12 +1,23 @@
-extends StaticBody2D
+#Code by: KeichiTS - 2023
 
+extends StaticBody2D
+var door_open = false
+var open_status = false
+
+func _process(delta):
+	if door_open == true and open_status == false :
+		$anim.play('opening')
+		await $anim.animation_finished
+		open_status = true
+	
+	elif door_open == false and open_status == true : 
+		$anim.play('closing')
+		await $anim.animation_finished
+		open_status = false
 
 
 func _close_the_door():
-	$anim.play("closing")
-	await $anim.animation_finished
+	door_open = false
 	
 func _open_the_door():
-	print('entrou')
-	$anim.play("opening")
-	await $anim.animation_finished
+	door_open = true 
