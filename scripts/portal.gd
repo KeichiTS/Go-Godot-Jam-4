7e37c62d60489b9944c7862dff46da747a716e66
KeichiTS <kkeichi@gmail.com>
2023-05-12 21:14:48 -0300

    More_levels

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -3,7 +3,8 @@
 extends Area2D
 
 @export_enum('Level_1', 'Level_2', 'Level_3', 'Level_4', 'Level_5', 'Level_6', 'Level_7',
-		'Level_8', 'Level_9', 'Level_10') var level: int
+		'Level_8', 'Level_9', 'Level_10', 'Level_11', 'Level_12', 'Level_13', 'Level_14',
+		'Level_15', 'Level_16', 'Level_17', 'Level_18', 'Level_19', 'Level_20') var level: int
 
 
 func _on_body_entered(body):
@@ -28,7 +29,29 @@ func _on_body_entered(body):
 			8:
 				get_tree().change_scene_to_file("res://scenes/Levels/level_10.tscn")
 			9:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_11.tscn")
+			10:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_12.tscn")
+			11:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_13.tscn")
+			12:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_14.tscn")
+			13:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_15.tscn")
+			14:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_16.tscn")
+			15:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_17.tscn")
+			16:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_18.tscn")
+			17:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_19.tscn")
+			18:
+				get_tree().change_scene_to_file("res://scenes/Levels/level_20.tscn")
+			19:
 				get_tree().change_scene_to_file("res://scenes/Levels/level_1.tscn")
+
+
 
 ###################################################
 #     ~ It ain't much, but it's honest work ~     #
